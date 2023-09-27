5319f603bbb29962bc7ace39b5e204f7023edde5
Guilherme Comachio Shimizu <gshimizu04@gmail.com>
2023-09-26 23:06:24 -0300

    Created classes Contador and ParametrosInvalidosException

---
+++ src/entities/Contador.java
@@ -0,0 +1,34 @@
+package entities;
+
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite o primeiro parâmetro: ");
+		int parametroUm = sc.nextInt();
+		System.out.print("Digite o segundo parâmetro: ");
+		int parametroDois = sc.nextInt();
+		
+		try {
+			contar(parametroUm, parametroDois);
+		}catch (ParametrosInvalidosException e) {
+			System.out.println(e.getMessage());;
+		}
+		
+		sc.close();
+	}
+	
+	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+		if (parametroUm > parametroDois) {
+			throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+		}
+		int contagem = parametroDois - parametroUm;
+		for (int i=1;i<=contagem;i++) {
+			System.out.println("Imprimindo o número " + i);
+		}
+
+	}
+}
+	
+
---
+++ src/entities/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+package entities;
+
+public class ParametrosInvalidosException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException(String message) {
+		super(message);
+	}
+}
